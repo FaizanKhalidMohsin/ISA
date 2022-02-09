203c9a7ff708917b0edcc6e8ac64edf045aceb6d
Faizan Khalid Mohsin <faizankms8@hotmail.com>
2022-02-09 01:12:12 -0500

    Update dataprep.R

--- dataprep.R
+++ dataprep.R
@@ -1,19 +1,6 @@
-# Dataprep
-
-
-
 library(tidyverse)
+source("Helpers.R")
 
-countryLookup = read_csv("ISA – WIDSR – Country Lookup.csv", n_max = 17, na = c("N/A")) %>%
-  select(Country:GGI)
-
-ourNamesInd = read_csv("ColumnNameLookupInd.csv")
-
-fix_gender_col <- function(x) {
-  case_when(str_detect(x, 'No one') ~ 'No One'
-            , x == "Female;Male" ~ "Both"
-            , TRUE ~ x)
-}
 
 sep_col <- function(dfr, colName = "PersonalEngagement") {
   
@@ -26,58 +13,45 @@ sep_col <- function(dfr, colName = "PersonalEngagement") {
     filter(value != "" | is.na(value)) %>%
     pivot_wider(id_cols = ID, names_from = value) %>% 
     select(-c("ID", "NA")) %>% 
-    mutate(across(everything(), ~replace(., !is.na(.), 1))) %>% # using replace() & replace_na() instead of ifelse() or if_else() due to speed.
+    # Using replace() & replace_na() instead of ifelse() or if_else() due to speed.
+    mutate(across(everything(), ~replace(., !is.na(.), 1))) %>%
     mutate(across(everything(), .fns = ~replace_na(.,0))) 
   
+  # Using Tyler the Great's naming convention.
+  sepColumnNames = new_columns_as_dataframe %>% 
+    colnames() %>%  str_to_title() %>% str_replace_all(pattern = " ", replacement = "_") %>% 
+    paste(str_to_upper(colName), ., sep = "_")
   
-  sepColumnNames = new_columns_as_dataframe %>% colnames() %>%  str_to_title() %>% str_replace_all(pattern = " ", replacement = "") %>% paste(colName, ., sep = "_")
   colnames(new_columns_as_dataframe) = sepColumnNames
   
   dfr = bind_cols(dfr, new_columns_as_dataframe)
   dfr
 }
 
-pretty_strings <- function(string) {
-  # string = "Marine Engineering and Technology" # Testing
-  pretty_new_lines = function(x) gsub("([^ ]+ [^ ]+) ", "\\1\n", x)
-  
-  blankCount = str_count(string, pattern = " ")
-  
-  for(i in 1:length(blankCount)){
-    thisBlankCount = blankCount[i]
-    # If only one space, replace with \n
-    if (thisBlankCount == 1) {
-      string[i] = str_replace(string[i], " ", "\n")
-      
-    } else if (thisBlankCount == 2) { # if 2 spaces, put beside longest word
-      string[i] = pretty_new_lines(string[i])
-      
-    } else if (thisBlankCount == 3) { # If 3 spaces, put after 2nd one
-      string[i] = pretty_new_lines(string[i])
-      
-    } else if (thisBlankCount > 3) { # If 4 or more, put every 2nd space
-      string[i] = pretty_new_lines(string[i])
-    }
-  }
-  return(string)
-}
 
+countryLookup = read_csv("ISA – WIDSR – Country Lookup.csv", n_max = 17, na = c("N/A")) %>%
+  select(Country:GGI)
 
-read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip=3, na = c(""," ", "N / A", "N/A")) %>%
+ourNamesInd = read_csv("ColumnNameLookupInd.csv")
+
+read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip = 3, na = c(""," ", "N / A")) %>%
   
   mutate( Country = str_to_title(Country)
           
           , NumPubs_PatentApps = as.numeric(NumPubs_PatentApps) # correct data type
           , NumPubs_Patents = as.numeric(NumPubs_Patents)
           
-          , Country = case_when(  str_detect(Country, 'Alice|Botsawana')       ~ 'Botswana'
-                                 , str_detect(Country, 'Trinidad|Tobago')       ~ "Trinidad &\nTobago" 
-                                 , str_detect(Country, 'Kitts|Nevis')           ~ "St. Kitts &\nNevis"        
-                                 , str_detect(Country, 'Maldives')              ~ "Maldives"
-                                 , str_detect(Country, 'Fiji')                  ~ "Fiji Islands"
-                                 , str_detect(Country, 'Mauritus')              ~ "Mauritius"
-                                 , str_detect(Country, 'Mocambique|Moçambique|Tabitha') ~ 'Mozambique'
-                                 , TRUE ~ Country)
+          , Country               = case_when(  str_detect(Country, 'Alice|Botsawana')       ~ 'Botswana'
+                                                , str_detect(Country, 'Trinidad|Tobago')       ~ "Trinidad & Tobago" 
+                                                , str_detect(Country, 'Kitts|Nevis')           ~ "St. Kitts & Nevis"        
+                                                , str_detect(Country, 'Maldives')              ~ "Maldives"
+                                                , str_detect(Country, 'Fiji')                  ~ "Fiji"
+                                                , str_detect(Country, 'Mauritus')              ~ "Mauritius"
+                                                , str_detect(Country, 'Mocambique|Moçambique|Tabitha') ~ 'Mozambique'
+                                                , TRUE ~ Country)
+          
+          , Age = fct_relevel(Age, c("18 - 24", "25 - 34"))
+          # , Field = pretty_strings(Field)
           
           , Helped_Research       = fix_gender_col(Helped_Research)
           , Helped_Grant          = fix_gender_col(Helped_Grant)
@@ -97,6 +71,10 @@ read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip=3, 
                                                        , str_detect(iPartnerWorkingForMoney, 'part') ~ 'Yes, part-time'
                                                        , TRUE ~ iPartnerWorkingForMoney)
           
+          , iPartnerWorkingForMoney_Ed     = case_when(str_detect(iPartnerWorkingForMoney_Ed, 'full') ~ 'Yes, full-time'
+                                                       , str_detect(iPartnerWorkingForMoney_Ed, 'part') ~ 'Yes, part-time'
+                                                       , TRUE ~ iPartnerWorkingForMoney_Ed)
+          
           , GendersTreatedEquallyAtWork    = case_when(str_detect(GendersTreatedEquallyAtWork, 'sometimes') ~ 'Sometimes'
                                                        , str_detect(GendersTreatedEquallyAtWork, 'No') ~ 'Consistent \nInequities'
                                                        , str_detect(GendersTreatedEquallyAtWork, 'Yes') ~ 'Always \nEquitable')
@@ -107,6 +85,8 @@ read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip=3, 
           
           , AwareGenderPayGap              = case_when(str_detect(AwareGenderPayGap, 'Yes') ~ 'Yes, men make more'
                                                        , TRUE ~ AwareGenderPayGap)
+          , AwareGenderPayGap_Ed           = case_when(str_detect(AwareGenderPayGap_Ed, 'Yes') ~ 'Yes, men make more' 
+                                                       , TRUE ~ AwareGenderPayGap_Ed)
           
           , iGendersTreatedEquallyAtSchool = case_when(str_detect(iGendersTreatedEquallyAtSchool, 'sometimes') ~ 'Sometimes'
                                                        , str_detect(iGendersTreatedEquallyAtSchool, 'No') ~ 'Consistent \nInequities'
@@ -114,9 +94,9 @@ read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip=3, 
           
           , iWomenMinorityOverlooked_Ed    = case_when(str_detect(iWomenMinorityOverlooked_Ed, 'No') ~  'Equitable \nTreatment'
                                                        , str_detect(iWomenMinorityOverlooked_Ed, 'Yes') ~  'Inequitable \nTreatment')
+          , iWomenMinorityOverlooked       = case_when(str_detect(iWomenMinorityOverlooked, 'No') ~  'Equitable \nTreatment'
+                                                       , str_detect(iWomenMinorityOverlooked, 'Yes') ~  'Inequitable \nTreatment')
           
-          , AwareGenderPayGap_Ed           = case_when(str_detect(AwareGenderPayGap_Ed, 'Yes') ~ 'Yes, men make more' 
-                                                       , TRUE ~ AwareGenderPayGap_Ed)
           
           , NumPubs_Articles               = if_else(NumPubs_Articles == 0.1, 0, NumPubs_Articles)
           , NumPubs_Books                  = if_else(NumPubs_Books == 0.1, 0, NumPubs_Books)
@@ -127,70 +107,82 @@ read_csv("Individual Survey.csv", col_names = ourNamesInd$InternalName, skip=3, 
           ## Try to replace the above NumPubs_* with one line code as below. 
           ## %>% mutate_at(vars(contains("NumPubs")), list(~case_when(.==0.1 ~ 0, TRUE ~ .x ) # Right now this .x does not work.
           
+          ## Coalesce all _Ed variables
+          , AwareGenderPayGap = coalesce(AwareGenderPayGap, AwareGenderPayGap_Ed)
+          , iDiscrimAtWork = coalesce(iDiscrimAtWork, iDiscrimAtWork_Ed)
+          , BasisWorkDiscrim = coalesce(BasisWorkDiscrim, BasisWorkDiscrim_Ed)
+          , RecipientLeers = coalesce(RecipientLeers, RecipientLeers_Ed)
+          
+          , DomesticArrangement = coalesce(DomesticArrangement, DomesticArrangement_Ed)
+          , iDependants = coalesce(iDependants, iDependants_Ed)
+          , ChildrenCaredForMethod = coalesce(ChildrenCaredForMethod, ChildrenCaredForMethod_Ed)
+          , iPartnerWorkingForMoney = coalesce(iPartnerWorkingForMoney, iPartnerWorkingForMoney_Ed)
+          
+          , iProgrammeNotCompleted = coalesce(iProgrammeNotCompleted, iProgrammeNotCompleted_Ed)
+          , ProgrammeNotCompleted_WhyNotComplete = coalesce(ProgrammeNotCompleted_WhyNotComplete, ProgrammeNotCompleted_WhyNotComplete_Ed)
+          , ProgrammeNotCompleted_Level = coalesce(ProgrammeNotCompleted_Level, ProgrammeNotCompleted_Level_Ed)
+          , ProgrammeNotCompleted_WhyEnrol = coalesce(ProgrammeNotCompleted_WhyEnrol, ProgrammeNotCompleted_WhyEnrol_Ed)
+          , NumHrsHousehold_Shopping = coalesce(NumHrsHousehold_Shopping, NumHrsHousehold_Shopping_Ed)
+          , NumHrsHousehold_Laundry = coalesce(NumHrsHousehold_Laundry, NumHrsHousehold_Laundry_Ed)
+          , NumHrsHousehold_HouseMaintenance = coalesce(NumHrsHousehold_HouseMaintenance, NumHrsHousehold_HouseMaintenance_Ed)
+          , NumHrsHousehold_Gardening = coalesce(NumHrsHousehold_Gardening, NumHrsHousehold_Gardening_Ed)
+          , NumHrsHousehold_Food = coalesce(NumHrsHousehold_Food, NumHrsHousehold_Food_Ed)
+          
+          , iWomenMinorityOverlooked = coalesce(iWomenMinorityOverlooked, iWomenMinorityOverlooked_Ed)
+          , ReasonsTurnDownTravel = coalesce(ReasonsTurnDownTravel, ReasonsTurnDownTravel_Ed)
+          , ActivityFrequency_SciencePres = coalesce(ActivityFrequency_SciencePres, ActivityFrequency_SciencePres_Ed)
+          , ActivityFrequency_RadioTV = coalesce(ActivityFrequency_RadioTV, ActivityFrequency_RadioTV_Ed)
+          , ActivityFrequency_NewspaperMagazine = coalesce(ActivityFrequency_NewspaperMagazine, ActivityFrequency_NewspaperMagazine_Ed)
+          , iWomenSuccessfulAsMen = coalesce(iWomenSuccessfulAsMen, iWomenSuccessfulAsMen_Ed)
+          , iOSEquallySuitedGender = coalesce(iOSEquallySuitedGender, iOSEquallySuitedGender_Ed)
+          
+          , iFundingApplied5Years = coalesce(iFundingApplied5Years, iFundingApplied5Years_Ed)
+          , GrantsResearchStay_National = coalesce(GrantsResearchStay_National, GrantsResearchStay_National_Ed)
+          , GrantsResearchStay_International = coalesce(GrantsResearchStay_International, GrantsResearchStay_International_Ed)
+          , GrantsAttendConf_National = coalesce(GrantsAttendConf_National, GrantsAttendConf_National_Ed)
+          , GrantsAttendConf_International = coalesce(GrantsAttendConf_International, GrantsAttendConf_International_Ed)
   ) %>% 
+  select(-ends_with("_Ed")) %>% # Remove '_Ed' variables now, they have been coalesced
+  
   ## All variables to de-coalesce and count
-  sep_col() %>%
-  sep_col(colName = "ReasonsForNotEnoughOppsConferences") %>%
-  sep_col(colName = "NatureWorkDiscrim") %>%
-  sep_col(colName = "BasisWorkDiscrim") %>%
-  sep_col(colName = "TrainingSubsequentActivity") %>%
-  sep_col(colName = "CareerProgressSupport") %>%
-  sep_col(colName = "ProgrammeNotCompleted_WhyEnrol") %>%
-  sep_col(colName = "ProgrammeNotCompleted_WhyNotComplete") %>%
-  sep_col(colName = "InterruptionReasons") %>%
-  sep_col(colName = "PositionsHeld") %>%
-  sep_col(colName = "ProgrammeNotCompleted_WhyEnrol_Ed") %>%
-  sep_col(colName = "ProgrammeNotCompleted_WhyNotComplete_Ed") %>%
-  sep_col(colName = "DifficultFulfillResp_Reasons") %>%
-  sep_col(colName = "ReasonsTurnDownTravel_Ed") %>%
-  sep_col(colName = "NatureWorkDiscrim_Ed") %>%
-  sep_col(colName = "BasisWorkDiscrim_Ed") %>%
-
-  # # select(-ends_with("_Ed")) %>%
+  # DONE: Faizan - These need to be prefixed with the original column name
+  # DONE: Faizan - Instead of removing spaces, replace with __ so that you can replace with a space downstream, otherwise it's unreadable
+  # TODO: Faizan - Clean up Grants* columns - some of the regions overlap and can now be combined
+  # TODO: Faizan - 
+  # TODO: Faizan - 
+  # TODO: Faizan - 
+  sep_col("PersonalEngagement") %>%
+  sep_col("PeopleInfluenced") %>%
+  sep_col("ReasonsForLeavingJob") %>%
+  sep_col("ReasonsForNotEnoughOppsConferences") %>%
+  sep_col("NatureWorkDiscrim") %>%
+  sep_col("BasisWorkDiscrim") %>%
+  sep_col("TrainingSubsequentActivity") %>%
+  sep_col("CareerProgressSupport") %>%
+  sep_col("ProgrammeNotCompleted_WhyEnrol") %>%
+  sep_col("ProgrammeNotCompleted_WhyNotComplete") %>%
+  sep_col("InterruptionReasons") %>%
+  sep_col("PositionsHeld") %>%
+  sep_col("DifficultFulfillResp_Reasons") %>%
+  sep_col("ReasonsTurnDownTravel") %>%
+  
   filter(Consent == "I consent") %>%
   filter(iStudyOrEmployed == "Yes") %>%
-  select(-c(Timestamp, Consent)) %>%
+  select(-c(Timestamp, Consent, iStudyOrEmployed)) %>%
   left_join(countryLookup, by = "Country") %>%
   saveRDS("ISA_Raw_Ind.rds")
 
 dd = readRDS("ISA_Raw_Ind.rds")
-colnames(dd)
-
-
-############################## Testing.  ##########################################
-
-n <- ncol(dd) 
-n
-dd[, (n - 10):n]
-
-colName = "NatureWorkDiscrim"
-new_columns_as_dataframe = dd %>%
-  pull(colName) %>%
-  str_split(pattern = ";", simplify = TRUE) %>%
-  as.data.frame() %>%
-  mutate(ID = row_number()) %>%
-  pivot_longer(starts_with("V")) %>%
-  filter(value != "" | is.na(value)) %>%
-  pivot_wider(id_cols = ID, names_from = value) %>% 
-  select(-c("ID", "NA")) %>% 
-  mutate(across(everything(), ~replace(., !is.na(.), 1))) %>% 
-  mutate(across(everything(), .fns = ~replace_na(.,0))) 
-
-
-sepColumnNames = new_columns_as_dataframe %>% colnames() %>%  str_to_title() %>% str_replace_all(pattern = " ", replacement = "") %>% paste(colName, ., sep = "_")
-colnames(new_columns_as_dataframe) = sepColumnNames
-
-dfr = bind_cols(dfr, new_columns_as_dataframe)
-dfr
-
-
-########################### Testing END ####################################
-
-dd %>% select(-c(starts_with("Helped_")
+dd$Field = pretty_strings(dd$Field)
+  
+dd %>% 
+  select(-c(starts_with("Helped_")
                  , starts_with("EmploymentSatisfaction_")
+                 , starts_with("PoliciesInOrg_")
                  , starts_with("OrgHavePolicies_")
                  , starts_with("Funding_")
                  , starts_with("InstitutionPolicies_")
+                 , starts_with("ActivityFrequency_")
                  )) %>% 
   saveRDS("ISA_Ind.rds")
 
@@ -220,9 +212,9 @@ read_csv("National Survey.csv") %>%
 
 
 table_plus <- function(n) {
-  print(colnames(dd)[n-1])
-  print(table(dd[[n-1]]))
-  print(colnames(dd)[n-1])
+  print(colnames(dd)[n - 1])
+  print(table(   dd[[n - 1]])   )
+  print(colnames(dd)[n - 1])
 }
 
 table_plus(3)
